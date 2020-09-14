62489d5f083f9680ac431a7506470a33e4de7056
Ji Hwan Lee <peter5659@naver.com>
2020-09-14 16:56:30 +0900

    leetcode Q3 revise

--- LeetCode/Q3_longest_substring_nonrepeat.py
+++ LeetCode/Q3_longest_substring_nonrepeat.py
@@ -20,10 +20,8 @@ class Solution(object):
                 print(last)
                 maxCurrent +=1
                 print(maxCurrent)
-                if maxCurrent>maxNum:
-                    maxNum = maxCurrent
-        if maxCurrent>maxNum:
-            maxNum = maxCurrent
+            if maxCurrent>maxNum:
+                maxNum = maxCurrent
         return(maxNum)
                         
 s= Solution()
@@ -32,10 +30,13 @@ length_of_subset = s.lengthOfLongestSubstring("abcdabc")
 print(length_of_subset)
 
 """
-python에서 문자열을 다른 언어에 비해 자유롭게 쓸수 있다는 점에서 편하게 생각할 수 있었다.
+python에서 문자열 편하게 생각할 수 있었다.
 문자열 붙이기를 사용하여 반복되지 않은 문자가 추가되었을때는 (기존문자열 + 새로운문자)로 추가
 반복된 문자가 추가 되었을때는 []인덱싱으로 추가해주었다.
 
-헷갈린 점 반복된 문자가 추가되었을 시에 인덱싱 범위를 정해주는 것에서 헷갈렸다. -> 처음에는 반복된 문자를 지워주었음. -> 이후 반복된 문자 뒤에서부터만 챙기면 된다는 것을 깨닫고 .find로 찾은 인덱스 뒤부터 [+1:]로 새로운 문자열을 생성해주었다.
-꼼꼼해야 할 점 문자열이 = " "일때. 문자열이 반복되는 것이 없을때.(기본형)
+헷갈린 점 반복되는 문자가 추가되었을 시에 새로운 문자열을 정의할 때 인덱싱 범위를 정해주는 것에서 헷갈렸다. 
+-> 처음에는 반복된 문자를 지워주었음. -> 이후 반복된 문자까지는 생각할 필요가 없고 뒤에서부터만 챙기면 된다는 것을 깨닫고 
+-> .find로 찾은 인덱스 뒤부터 [+1:]로 새로운 문자열을 생성해주었다.
+꼼꼼해야 할 점 
+문자열이 = " "일때. 문자열이 반복되는 것이 없을때.(기본형)
 """
